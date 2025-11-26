4117b413ca3b0af6ac1d5445edd5512d56cb1b8e
Francovasquezz <135084946+Francovasquezz@users.noreply.github.com>
2025-11-26 18:03:04 -0300

    fix de port db

--- backend/db.py
+++ backend/db.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "6543")
+DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "postgres")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
